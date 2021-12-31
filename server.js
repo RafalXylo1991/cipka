2f8cb28210773cd608202ede524677e096faef30
hunter <xylohunter@gmail.com>
2021-12-31 16:01:43 +0100

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const Datastore=require('nedb');
 const port =process.env.PORT || 3000
 const app = express();
 app.set('view engine','hbs')
 app.get('/',(req,res)=> {
-    res.render('index',{
-        pageTitle:"cycki"
-    })
+
+let x;
+const database = new Datastore('Persons.db');
+database.loadDatabase();
+database.insert({name:'Jada Facer',age:23});
+database.findOne({ name: 'Jada Facer' }, function(err, doc) {
+  console.log('Found user:', doc.name);
+  res.render('index',{
+
+    cycki:doc.name
+  })
+  
+});
+
 
 })
 app.listen(port);
